c877d18c150fe4449bbed0dc1c4f2bfcad9098be
hardik-crest <hchavda@splunk.com>
2023-04-06 11:45:37 +0530

    resolved compiled test

--- awsdynamodb_consts.py
+++ awsdynamodb_consts.py
@@ -22,7 +22,7 @@ AWS_DYNAMODB_REGIONS = {
     "US East (Ohio)": "us-east-2",
     "US East (N. Virginia)": "us-east-1",
     "US West (N. California)": "us-west-1",
-    "US West (Oregon)":	"us-west-2",
+    "US West (Oregon)": "us-west-2",
     "Africa (Cape Town)": "af-south-1",
     "Asia Pacific (Hong Kong)": "ap-east-1",
     "Asia Pacific (Hyderabad)": "ap-south-2",
